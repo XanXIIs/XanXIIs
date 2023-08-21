962a465daea9f9fdaf5044a1996bc3d67034d92f
Gabriel Sanches <gabrielsanches98@hotmail.com>
2023-08-21 14:46:33 +0000

    Estrutura IF

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//condicional
+var flag = 25;
+
+if(flag > 50) {
+    Console.WriteLine("Maior");
+}  else {
+    Console.WriteLine ("não é Maior");
+}
+
+switch (flag) {
+    case 50:
+    Console.WriteLine("valor igual 50");
+    break;
+}
